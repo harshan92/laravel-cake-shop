e68d90b7193dadd8d320b48e7f37f2d601b0b276
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2022-09-11 16:01:48 +0530

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,60 @@
+ <!-- info section -->
+ <section class="info_section layout_padding">
+    <div class="container">
+      <div class="info_social">
+        <div>
+          <a href="">
+            <img src="{{ asset('images/fb.png') }}" alt="">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="{{ asset('images/twitter.png') }}" alt="">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="{{ asset('images/linkedin.png') }}" alt="">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="{{ asset('images/instagram.png') }}" alt="">
+          </a>
+        </div>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <!-- end info_section -->
+
+  <!-- footer section -->
+  <section class="container-fluid footer_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 col-md-9 mx-auto">
+          <p>
+            &copy; <span id="currentDate"></span> All Rights Reserved By
+            <a href="https://html.design/">Free Html Templates</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- footer section -->
+
+
+  <script type="text/javascript" src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
+
+
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Cakejoey</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css') }}" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Lobster|Poppins:400,600,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="{{ asset('css/responsive.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <img src="{{ asset('images/logo.png') }}" alt="">
+            <span>
+              Cakejoey
+            </span>
+          </a>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="s-1"> </span>
+            <span class="s-2"> </span>
+            <span class="s-3"> </span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.html"> About </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="product.html"> Products </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link last_link" href="contact.html"> Contact </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
